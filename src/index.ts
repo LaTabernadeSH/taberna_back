be533a134bba6d9561986511d07cddae4b43826f
carlitosmunkstar <camobaan@gmail.com>
2023-07-07 10:46:59 -0500

    primer commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const PORT = 3000
+
+app.get('ping', (_req, res) => {
+    console.log('someone pinged here!!')
+    res.send('pong')
+})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
