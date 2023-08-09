09fa944334b564e8e1c28281f0a165d944ecec50
Fernanda Olivares <folivareslisperguer@gmail.com>
2023-08-09 17:36:28 -0400

    Update Model Post

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-	env: {
-		commonjs: true,
-		es2021: true,
-		node: true,
-	},
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [".eslintrc.js"],
-			parserOptions: {
-				sourceType: "script",
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 2021,
-	},
-	rules: {
-		indent: ["error", "tab"],
-		"no-tabs": "off",
-		"no-trailing-spaces": "error",
-		"linebreak-style": ["error", "windows"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
-	},
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.js"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    indent: ["error", 2],
+    "no-tabs": "off",
+    "no-trailing-spaces": "error",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
