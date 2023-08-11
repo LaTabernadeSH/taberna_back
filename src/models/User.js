309655722a2033fb880f190233828514978e5ca7
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-08-11 11:37:47 -0300

    update model user

--- src/models/User.js
+++ src/models/User.js
@@ -1,37 +1,63 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-	sequelize.define(
-		"User",
-		{
-			id: {
-				type: DataTypes.UUID,
-				defaultValue: DataTypes.UUIDV4,
-				primaryKey: true,
-				allowNull: false,
-				auto_increment: true,
-				unique: true,
-			},
-			name: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},lastName: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			mail:{
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			userName:{
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			password:{
-				type: DataTypes.STRING,
-				allowNull: false,
-			}
-		},
-		{ timestamps: false }
-	);
+  sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      //contact
+      linkedin: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      github: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      twitter: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      whatsapp: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    { timestamps: false }
+  );
 };
