c26c5adceed773162cd70e8c19ff801c0da42f7a
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-08-24 19:36:26 -0300

    feature(middleware: handleErrors)

---
+++ src/routes/middleware/handleErrors.js
@@ -0,0 +1,34 @@
+const ERROR_HANDLERS = {
+  CastError: (res) => {
+    res.status(400).json({ error: "id used is malformed" }).end();
+  },
+
+  JsonWebTokenError: (res) => {
+    res.status(401).json({ error: "token missing or invalid" }).end();
+  },
+
+  TokenExpirerError: (res) => {
+    res.status(401).json({ error: "token expired" }).end();
+  },
+
+  SequelizeDatabaseError: (res, err) => {
+    res.status(409).json({ error: err.message }).end();
+  },
+
+  ValidationError: (res, err) => {
+    res.status(409).json({ error: err.message }).end();
+  },
+
+  defaultError: (res, err) => {
+    const status = err.status || 500;
+    const message = { error: err.message } || err;
+    //console.error(err);
+    res.status(status).json(message).end();
+  },
+};
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (err, req, res, next) => {
+  const handler = ERROR_HANDLERS[err.name] || ERROR_HANDLERS.defaultError;
+  handler(res, err);
+};
